21b5ff17579b6f307c5a312683029baa7f47f709
Anton Akhankov <70724531+aakhankov@users.noreply.github.com>
2022-08-21 14:17:03 +0300

    module7-task1

---
+++ js/pictures.js
@@ -0,0 +1,21 @@
+import { createObject } from "./objects.js";
+
+const picturesBlock = document.querySelector('.pictures'); //блок картинок
+
+const picturesTemplate = document.querySelector('#picture').content; // Фрагмент содержимого шаблона картинок
+
+const pictures = createObject();
+
+const picturesFragment = document.createDocumentFragment();
+
+pictures.forEach(({url, comments, likes})=>{
+    const picturesElement = picturesTemplate.cloneNode(true);
+    picturesElement.querySelector('.picture__img').src = url;
+    picturesElement.querySelector('.picture__comments').textContent = comments.length;
+    picturesElement.querySelector('.picture__likes').textContent = likes;
+    picturesFragment.appendChild(picturesElement);
+});
+
+picturesBlock.appendChild(picturesFragment);
+
+export{picturesBlock, pictures};
