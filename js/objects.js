83cf42d6a1da32a3f08eaf4f1013c88299c56843
Anton Akhankov <70724531+aakhankov@users.noreply.github.com>
2022-08-21 14:14:11 +0300

    module5-task1

---
+++ js/objects.js
@@ -0,0 +1,17 @@
+import {getRandomIntFromRange, getRandomArrayElement} from './util.js';
+import {DESCRIPTIONS, OBJECTS_COUNT} from './data.js';
+import {createComment} from './comments.js';
+
+// Функция для создания объектов (фотографий)
+function createObject() {
+  const arrayObjects = new Array(OBJECTS_COUNT).fill().map((j, index) => ({
+    id: index + 1,
+    url: `photos/${index + 1}.jpg`,
+    description: getRandomArrayElement(DESCRIPTIONS),
+    likes: getRandomIntFromRange(15, 200),
+    comments: createComment(),
+  }));
+  return arrayObjects;
+}
+
+console.log(createObject()); // Временный вызов функции в консоль
---
+++ js/util.js
@@ -0,0 +1,16 @@
+// Функция, возвращающая случайное целое число из переданного диапазона включительно
+const getRandomIntFromRange = (from, to) => {
+  from = Math.ceil(from);
+  to = Math.floor(to);
+  return Math.floor(Math.random() * (to - from + 1)) + from;
+};
+
+// Функция для проверки максимальной длины строки.
+const checkStringMaxLength = (string, maxLength) => string.length <= maxLength;
+
+checkStringMaxLength('hello', 10); // Временный вызов функции
+
+// Функция поиска случайного элемента массива
+const getRandomArrayElement = (elements) => elements[getRandomIntFromRange(0, elements.length - 1)];
+
+export {getRandomIntFromRange, getRandomArrayElement};
